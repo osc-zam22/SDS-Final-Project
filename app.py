3e66f047d857f07cd1b7cd7398cd2ca77a7db62c
Oscar Zambrano <oscar@techexchange.in>
2022-04-13 19:11:43 +0000

    added some html and some app routes

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask , redirect , render_template , session , url_for
-from flask_pymongo import Pymongo
+from flask import Flask, request, redirect, render_template, session , url_for
+from flask_pymongo import PyMongo
 import pymongo
 from bson.objectid import ObjectId
 import secrets
 import bcrypt
 import random
 
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/directory')
+def directory():
+    return render_template('directory.html')
