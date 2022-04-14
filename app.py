f51d4b8cd80bec5b8636efbee196ad479a2d0fa3
Oscar Zambrano <oscar@techexchange.in>
2022-04-14 03:25:43 +0000

    added log in and sign up funcitonality

--- app.py
+++ app.py
@@ -33,12 +33,69 @@ def directory():
     return render_template('directory.html')
 
 @app.route('/login')
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        users = db.users
 
-@app.route('/signup')
+        user = users.find_one({"email": request.form["email"]})
+
+        #if user with email exists
+        if user:
+            #Gets actual password tied to user
+            actual_password = user['password']
+            #encodes attempted password
+            attempted_password = request.form['password'].encode("utf-8")
+
+            if bcrypt.checkpw(attempted_password, actual_password):
+                #store in session
+                session['email'] = request.form['email']
+                return redirect(url_for('index'))
+            else:
+                return render_template('login.html', prompt = "Invalid password... try again.")
+        
+        else:
+            return render_template('login.html', prompt = "User not found... try again.")
+    else:
+        return render_template('login.html')
+
+
+@app.route('/signup' , methods = ['GET' , 'POST'])
 def signup():
-    return render_template('signup.html')
+    app.route('/signup' , methods = ['GET' , 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        users = db.users
+
+        existing_user = users.find_one({"email": request.form['email']})
+
+        if not existing_user:
+            username = request.form['username']
+            email = request.form['email']
+            
+            #encrypting password
+            password = request.form['password'].encode("utf-8")
+
+            #Makes sure all fields were filled in
+            if not username or not email or not password:
+                return render_template("signup.html", prompt = "Invalid sign up. Make sure to fill in every field.")
+            #hashing password
+            salt = bcrypt.gensalt()
+            hashed_password = bcrypt.hashpw(password, salt)
+
+            #adding to users database
+            users.insert_one({'userame': username, 'email': email, 'password': hashed_password})
+
+            #creating new session
+            session['email'] = request.form['email']
+
+            return redirect(url_for('index'))
+
+        else:
+            return render_template("signup.html", prompt = "username is already registered... try logging in.")
+    else:
+        return render_template("signup.html")
+    
 
 @app.route('/thread')
 def thread():
